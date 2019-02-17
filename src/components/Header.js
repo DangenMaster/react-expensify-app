e51541ac2a87cb1cdf4781bc144cc880f6f48ee9
DangenMaster <dangenmaster02@gmail.com>
2019-02-18 03:24:06 +0530

    Removing Navlinks for edit expense and help page from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,6 @@ const Header = () => (
     <h1>Expensify</h1>
     <NavLink exact to="/" activeClassName="is-active">Dashboard</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
   </header>
 );
 
